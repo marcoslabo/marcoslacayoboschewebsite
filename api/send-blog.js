1ece6a764903deaf8263fd524ddd5d8670e9e524
Marcos Lacayo Bosche <marcoslacayobosche@gmail.com>
2026-02-23 07:41:47 -0600

    fix: remove unverified replyTo to fix Brevo 500 error

--- api/send-blog.js
+++ api/send-blog.js
@@ -23,10 +23,7 @@ export default async function handler(req, res) {
                 name: 'Marcos Bosche',
                 email: 'marcos@marcoslacayobosche.com'
             },
-            replyTo: {
-                name: 'Marcos Bosche',
-                email: 'marcos.bosche@nymbl.app'
-            },
+
             type: 'classic',
             htmlContent: buildEmailHtml(title, content, excerpt, blogUrl),
             recipients: {
